0e4fb029dcb9e0f32102dc06273d5f0144475b55
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-05 15:44:34 -0700

    commit tree 5ebee9bdd5ff6f44cee5b53fe5b6d23b43f08c72

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :anagram
+    def initialize(anagram)
+        @anagram = anagram
+    end
+    def match(array)
+        temp = []
+        array.each {|word| temp << word if word.split("").sort == @anagram.split("").sort}
+        temp
+    end
+end
